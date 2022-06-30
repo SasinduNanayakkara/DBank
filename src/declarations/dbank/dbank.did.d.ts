80924d63a1cf393c1709481e5df0b278c0d7e9f7
SasinduNanayakkara <it20625016@my.sliit.lk>
2022-07-01 04:24:53 +0530

    Implement compund function & chnage data types

--- src/declarations/dbank/dbank.did.d.ts
+++ src/declarations/dbank/dbank.did.d.ts
@@ -2,7 +2,8 @@ import type { Principal } from '@dfinity/principal';
 import type { ActorMethod } from '@dfinity/agent';
 
 export interface _SERVICE {
-  'checkBalance' : ActorMethod<[], bigint>,
-  'topUp' : ActorMethod<[bigint], undefined>,
-  'withdrawl' : ActorMethod<[bigint], undefined>,
+  'checkBalance' : ActorMethod<[], number>,
+  'compound' : ActorMethod<[], undefined>,
+  'topUp' : ActorMethod<[number], undefined>,
+  'withdrawl' : ActorMethod<[number], undefined>,
 }
